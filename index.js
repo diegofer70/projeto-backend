631a943a684c907bbba70caecb866743bc9d680c
DIEGO FERREIRA DE ARAUJO <DIEGO6040@DIGITALCOLLEGE.LOCAL>
2024-08-08 10:43:15 -0300

    chuchu-atualizando

--- index.js
+++ index.js
@@ -2,21 +2,69 @@ const express = require('express')
 var cors = require('cors')
 // DESESTRUTURACAO
 const { Sequelize, DataTypes, QueryTypes } = require('sequelize');
+const { database } = require('pg/lib/defaults');
 const app = express()
 const port = 3000
 
-const sequelize = new Sequelize('postgres', 'postgres.yhogvuqcegpuxhumgkyn', '', {
-    host: "aws-0-us-west-1.pooler.supabase.com",
-    dialect: 'postgres',
-    port: 5432,
-    logging: true, // Desative o log de SQL se não precisar
-});
+const sequelize = new Sequelize('postgresql://postgres.udontlftvjxyhqwnrxji:' + 'projeto-backend' + 'aws-0-us-west-1.pooler.supabase.com:6543/postgres');
+
+const User = sequelize.define(
+    'User',
+    {
+        // Model attributes are defined here
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        surname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        timestamps: true
+    },
+);
+
+const Category = sequelize.define(
+    'Category',
+    {
+        // Model attributes are defined here
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        slug: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        use_in_menu: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: false
+        },
+    },
+    {
+        timestamps: true
+    },
+);
+
+sequelize.sync();
+
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Olá, mundo')
+    res.send('Clicar produtos')
 })
-// /v1/user/chuchu METODOS HTTP
+// /v1/user/nome METODOS HTTP
 
 app.get('/v1/user/:id', (request, res) => {
     console.log('request.url', request.url) // debug
@@ -26,13 +74,21 @@ app.get('/v1/user/:id', (request, res) => {
         type: QueryTypes.SELECT,
     }).then((result) => res.send(result));
 })
-app.post('/v1/user/:name', (request, res) => {
-    console.log('request.url', request.url) // debug
-    console.log('request.params.name', request.params.name)
+// app.post('/v1/user/:name', (request, res) => {
+//     console.log('request.url', request.url) // debug
+//     console.log('request.params.name', request.params.name)
 
-    sequelize.query("INSERT INTO users (name, email) values ('"+request.params.name+"', '"+request.params.name+"')", {
-        type: QueryTypes.INSERT,
-    }).then((result) => res.status(201).send(result));
+//     sequelize.query("INSERT INTO users (name, email) values ('" + request.params.name + "', '" + request.params.name + "')", {
+//         type: QueryTypes.INSERT,
+//     }).then((result) => res.status(201).send(result));
+// })
+
+app.post('/v1/user', (request, res) => {
+    console.log('request.url', request.url) // debug
+    console.log('request.body', request.body)
+    // res.send(request.body)
+    User.create(request.body).then((result) => res.status(201).send(result))
+
 })
 
 app.listen(port, () => {
