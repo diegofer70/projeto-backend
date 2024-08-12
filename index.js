f301cba1aa165935835ea5d01ee2cfb53f126d39
diego araujo <ferreiradearaujod@gmail.com>
2024-08-12 07:51:26 -0300

    ajustando

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')    
+var cors = require('cors')
+var bodyParser = require('body-parser')
 // DESESTRUTURACAO
 const { Sequelize, DataTypes, QueryTypes } = require('sequelize');
 const app = express()
 const port = 10000
 
 const sequelize = new Sequelize('postgresql://postgres.hspsjnkdtvcaiuxplsov:chuchu-diego-araujo@aws-0-us-west-1.pooler.supabase.com:6543/postgres');
-// temos 3 formas de conectar o banco, vamos tentar outra forma 
 
 const User = sequelize.define(
     'User',
     {
-        // Model attributes are defined here
-        firstName: {
+        firstname: {
             type: DataTypes.STRING,
             allowNull: false,
         },
         surname: {
             type: DataTypes.STRING,
-            allowNull: false,
+            allowNull: false
         },
         email: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true
         },
         password: {
             type: DataTypes.STRING,
-            allowNull: false,
+            allowNull: false
         }
     },
     {
@@ -35,20 +34,20 @@ const User = sequelize.define(
     },
 );
 
+
 const Category = sequelize.define(
     'Category',
     {
-        // Model attributes are defined here
         name: {
             type: DataTypes.STRING,
             allowNull: false,
         },
         slug: {
             type: DataTypes.STRING,
-            allowNull: false,
+            allowNull: false
         },
         use_in_menu: {
-            type: DataTypes.STRING,
+            type: DataTypes.BOOLEAN,
             allowNull: false,
             defaultValue: false
         },
@@ -58,28 +57,35 @@ const Category = sequelize.define(
     },
 );
 
-sequelize.sync({alter: true})
-.then (() => {
-    console.log('Connection has been established successfully.');
-})  
-   .catch (error => {
-  console.error('Unable to connect to the database:', error);
-});
+
+
+// Sincronizar o modelo com o banco de dados    
+sequelize.sync();
 
 app.use(bodyParser.json()) // middleware
 app.use(cors()) // middleware
 
+
 app.get('/', (req, res) => {
-    res.send('Olá, mundo!')
+    res.send('Olá, mundo')
 })
-// /v1/user/nome METODOS HTTP
+// /v1/user/chuchu METODOS HTTP
+
+// app.get('/v1/user/:id', (request, res) => {
+//     console.log('request.url', request.url) // debug
+//     console.log('request.params.id', request.params.id)
+
+//     sequelize.query("SELECT * FROM users where id='" + request.params.id + "'", {
+//         type: QueryTypes.SELECT,
+//     }).then((result) => res.send(result));
+// })
 
 app.get('/v1/user/:id', (request, res) => {
     console.log('request.url', request.url) // debug
     console.log('request.params.id', request.params.id)
 
-    User.findOne({ where: { id: request.params.id }})
-    .then((result) => res.send(result));
+    User.findOne({ where: { id: request.params.id } })
+        .then((result) => res.send(result))
 })
 // app.post('/v1/user/:name', (request, res) => {
 //     console.log('request.url', request.url) // debug
@@ -96,7 +102,14 @@ app.post('/v1/user', (request, res) => {
     // res.send(request.body)
     User.create(request.body).then((result) => res.status(201).send(result))
 
+    // User.create({ 
+    //     firstname: request.params.name, 
+    //     surname: request.params.name, 
+    //     email: request.params.name, 
+    //     password: request.params.name })
+    //     .then((result) => res.status(201).send(result));
 })
+
 
 app.put('/v1/user/:id', (request, res) => {
     console.log('request.url', request.url) // debug
@@ -106,10 +119,11 @@ app.put('/v1/user/:id', (request, res) => {
 
 app.delete('/v1/user/:id', (request, res) => {
     console.log('request.url', request.url) // debug
-    User.destroy({ where: { id: request.params.id } }).then((result) => res.send(result))
+    User.destroy({ where: { id: request.params.id } }).then((result) => {
+        res.send('deletei com sucesso essa quantidade de linhas: '+result)
+    })
 })
 
 app.listen(port, () => {
-    console.log(`Exemplo de aplicativo escutando na porta ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
-
---
+++ src/app.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const jwt = require('jsonwebtoken');
+
+require('./routes/authRoutes')
+require('./routes/userRoutes')
+require('./routes/productRoutes')
+
+const app = require('./routes/app-express')
+const PORT = process.env.PORT || 10000
+
+app.listen(PORT, ()=> { console.log('chuchu atomico')})
