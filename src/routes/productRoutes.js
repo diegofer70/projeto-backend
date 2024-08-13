03022e56598e78f158448ed599e24c53b9f0f942
diego araujo <ferreiradearaujod@gmail.com>
2024-08-13 11:44:24 -0300

    feat: teste-codigo

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
     res.send('Olá, mundo')
 })
 
+app.get('/v1/product/search', (request, res) => {
+    console.log(req.query)
+    const produtos = await Product.findAll({limit: 30});
+
+    Product.findAll({ where: { id: request.params.id } })
+    res.send(produtos))
+})
+
 app.get('/v1/product/:id', (request, res) => {
     console.log('request.url', request.url) // debug
     console.log('request.params.id', request.params.id)
@@ -15,14 +23,16 @@ app.get('/v1/product/:id', (request, res) => {
 })
 
 app.post('/v1/product', (request, res) => {
-    Product.findAll()
+    console.log('request.url', request.url) // debug
+    console.log('request.body', request.body)
+
+    Product.create(request.body).then((result) => res.status(201).send(result))
 })
 
 app.put('/v1/product/:id', (request, res) => {
     console.log('request.url', request.url) // debug
-
-    Product.update()
-    .then((result) => res.send(result))
+    console.log('request.body', request.body)
+    Product.update(request.body, { where: { id: request.params.id } }).then((result) => res.send(result))
 })
 
 app.delete('/v1/product/:id', (request, res) => {
