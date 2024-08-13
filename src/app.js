be44668524614bbf8f15957541e514138c5e9a72
diego araujo <ferreiradearaujod@gmail.com>
2024-08-13 09:09:13 -0300

    feat: criei um env arquivo

--- src/app.js
+++ src/app.js
@@ -10,4 +10,4 @@ require('./routes/categoryRoutes')
 const app = require('./routes/app-express')
 const PORT = process.env.PORT || 10000
 
-app.listen(PORT, ()=> { console.log('chuchu atomico')})
+app.listen(PORT, ()=> { console.log('Bomba atomico')})
