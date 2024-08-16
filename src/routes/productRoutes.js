05c6e71c4b3a873045c73814fd2be64a91c7ac08
diego araujo <ferreiradearaujod@gmail.com>
2024-08-16 11:35:09 -0300

    feat: ajustando

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.send('Olá, mundo')
 })
 
-app.get('/v1/product/search', async (req, res) => {
+app.get('/v1/product/search', async (request, res) => {
     console.log(req.query)
 
     const produtos = await Product.findAll({limit: 30});
@@ -15,25 +15,24 @@ app.get('/v1/product/search', async (req, res) => {
     res.send(produtos)
 })
 
-app.get('/v1/product/:id', (request, res) => {
-    console.log('request.url', request.url) // debug
-    console.log('request.params.id', request.params.id)
+app.get("/v1/product/:id", (request, res) => {
+    console.log("request.url", request.url); // debug
+    console.log("request.params.id", request.params.id);
+  
+    Product.findOne({ where: { id: request.params.id } }).then((result) =>
+      res.send(result)
+    );
+  });
 
-    Product.findOne({ where: { id: request.params.id } })
-        .then((result) => res.send(result))
-})
-
-app.get('/v1/product/', (request, res) => {
-    console.log('request.url', request.url) // debug
-
-
-    Product.findAll()
-        .then((result) => res.send(result))
-})
+app.get("/v1/product/", (request, res) => {
+    console.log("request.url", request.url); // debug
+  
+    Product.findAll().then((result) => res.send(result));
+});
 
 app.post('/v1/product', (request, res) => {
-    console.log('request.url', request.url) // debug
-    console.log('request.body', request.body)
+    console.log('request.url', request.url); // debug
+    console.log('request.body', request.body);
 
     Product.create(request.body).then((result) => res.status(201).send(result))
 })
